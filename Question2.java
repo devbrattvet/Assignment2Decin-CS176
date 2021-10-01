78159d64980f457360e2bc451d81ea370f2da5d4
devbrattvet <71794506+devbrattvet@users.noreply.github.com>
2021-09-30 23:09:37 -0400

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		
+		
+		in.close();
+	}
+
+}
